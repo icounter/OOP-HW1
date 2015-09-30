47fef6235c844a9dd7fd89a77e9d602ea85036d9
“peterchao” <peterchaoz93@gmail.com>
2015-09-30 09:16:33 -0400

    CommitInfo

--- MTH9815HW01Q2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#S20130124_subset.txt needs to be doadloaded in advance
-#type ./MTH9815HW01Q2.sh "symbol" when using the code
-mkdir -p $1
-cd $1
-touch \<$1\>_20130124.txt
-cd ..
-grep -E "\s$1\s" S20130124_subset.txt > ./$1/\<$1\>_20130124.txt
-
